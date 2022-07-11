96fda615a0cb296ef5f61345d46be1a036ddf22c
LAPTOP-U2EODQFC\Lenovo <linh.hnse1998@gmail.com>
2022-07-11 22:40:06 +0700

    first commit

---
+++ taco-cloud/src/test/java/com/linhhnse/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhhnse.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
